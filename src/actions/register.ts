805fdbd0a28cf54c6578f63c4bf8956fb71f8516
Claudio Lins <claudio.lins@me.com>
2024-09-22 21:41:07 +0100

    core: Registe action

--- src/actions/register.ts
+++ src/actions/register.ts
@@ -1,5 +1,8 @@
 'use server'
 
+import { prisma } from '@/lib/prisma'
+import bcryptjs from 'bcryptjs'
+
 import { RegisterSchema } from '@/schemas'
 import type { z } from 'zod'
 
@@ -10,5 +13,26 @@ export async function register(values: z.infer<typeof RegisterSchema>) {
 	if (!validatedFields.success) {
 		return { error: 'Invalid fields!' }
 	}
+	const { name, email, password } = validatedFields.data
+	const hashedPassword = await bcryptjs.hash(password, 10)
+
+	const existingUser = await prisma.user.findUnique({
+		where: { email },
+	})
+
+	if (existingUser) {
+		return { error: 'Email already exists!' }
+	}
+
+	await prisma.user.create({
+		data: {
+			name,
+			email,
+			password: hashedPassword,
+		},
+	})
+
+	// Send verification email
+
 	return { success: 'Email sent successfully!' }
 }
