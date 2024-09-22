9585cd1c4f47cbc428245e23e1151bbb793f3bb8
Claudio Lins <claudio.lins@me.com>
2024-09-22 18:57:39 +0100

    feat: create register form

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -4,3 +4,10 @@ export const LoginSchema = z.object({
 	email: z.string().email(),
 	password: z.string(),
 })
+
+export const RegisterSchema = z.object({
+	name: z.string().min(3, 'Name must be at least 3 characters'),
+	email: z.string().email(),
+	password: z.string().min(6, 'Password must be at least 6 characters'),
+	// confirmPassword: z.string(),
+})
